46cb584f7818b7c0a46b1e6c76c3599f813e53bc
Pat O'Callaghan <pat@intercom.io>
2019-12-13 09:25:49 +0000

    Opt out moving component templates used as partials or in layoutName

--- test/fixtures/classic-app/output.js
+++ test/fixtures/classic-app/output.js
@@ -11,10 +11,26 @@ module.exports = {
           'super-nested-component.hbs': '{{!-- nested1/nested2/super-nested-component.hbs --}}',
           'super-nested-component.js': '// nested1/nested2/super-nested-component.js'
         }
+      },
+      'layout-name': {
+        'has-layout-name.js': `// top-level-component.js
+        Component.extend({ layoutName: "components/layout-name/layout-name-template" });`,
+      },
+      'partials': {
+        'with-partial.hbs': `{{!-- with-partial.hbs --}}
+          {{partial 'components/partials/partials-template'}}`,
       }
     },
     templates: {
       'application.hbs': '{{outlet}}',
+      components: {
+        'layout-name': {
+          'layout-name-template.hbs': '{{!-- layout-name-template.hbs --}}',
+        },
+        'partials': {
+          'partials-template.hbs': '{{!-- partials-template.hbs --}}'
+        }
+      }
     }
   },
 };
