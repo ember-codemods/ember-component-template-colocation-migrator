79b0eafc505c81fb5644ab56c8f66f56a0224ff5
ijlee2 <ijlee2@users.noreply.github.com>
2020-04-26 10:29:18 -0500

    Added test examples of a "typical" nested component and a template-only component

--- test/fixtures/classic-to-flat/output.js
+++ test/fixtures/classic-to-flat/output.js
@@ -1,9 +1,28 @@
 module.exports = {
   app: {
     'app.js': '// app',
+
     components: {
+      // A standalone component
       'top-level-component.hbs': '{{!-- top-level-component.hbs --}}',
       'top-level-component.js': '// top-level-component.js',
+
+      // A template-only component
+      'template-only-component.hbs': '{{!-- template-only-component.hbs --}}',
+
+      // A nested component
+      'parent-component.hbs': '{{!-- parent-component.hbs --}}',
+      'parent-component.js': '// parent-component.js',
+      'parent-component': {
+        'child-component.hbs': '{{!-- parent-component/child-component.hbs --}}',
+        'child-component.js': '// parent-component/child-component.js',
+        'child-component': {
+          'grandchild-component.hbs': '{{!-- parent-component/child-component/grandchild-component.hbs --}}',
+          'grandchild-component.js': '// parent-component/child-component/grandchild-component.js'
+        }
+      },
+
+      // Another nested component
       nested1: {
         'nested-component.hbs': '{{!-- nested1/nested-component.hbs --}}',
         'nested-component.js': '// nested1/nested-component.js',
@@ -12,23 +31,36 @@ module.exports = {
           'super-nested-component.js': '// nested1/nested2/super-nested-component.js'
         }
       },
+
+      // A component with layoutName
       'layout-name': {
-        'has-layout-name.js': `// top-level-component.js
-        Component.extend({ layoutName: "components/layout-name/layout-name-template" });`,
+        'has-layout-name.js': [
+          '// top-level-component.js',
+          'Component.extend({ layoutName: "components/layout-name/layout-name-template" });'
+        ].join('\n')
       },
+
+      // A component with partial
       'partials': {
-        'with-partial.hbs': `{{!-- with-partial.hbs --}}
-          {{partial 'components/partials/partials-template'}}`,
+        'with-partial.hbs': [
+          '{{!-- with-partial.hbs --}}',
+          '{{partial "components/partials/partials-template"}}'
+        ].join('\n')
       }
     },
+
     templates: {
       'application.hbs': '{{outlet}}',
+
       components: {
+        // A component with layoutName
         'layout-name': {
-          'layout-name-template.hbs': '{{!-- layout-name-template.hbs --}}',
+          'layout-name-template.hbs': '{{!-- layout-name-template.hbs --}}'
         },
+
+        // A partial template
         'partials': {
-          'partials-template.hbs': '{{!-- partials-template.hbs --}}'
+          'partials-template.hbs': '{{!-- partials-template.hbs --}}',
         }
       }
     }
