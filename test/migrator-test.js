f534b56c159221d17fde33a7bc26c269f30757ba
ijlee2 <ijlee2@users.noreply.github.com>
2020-04-26 18:43:34 -0500

    Created changeComponentStructureToNested method

--- test/migrator-test.js
+++ test/migrator-test.js
@@ -57,4 +57,53 @@ describe("Migrator", function() {
       });
     });
   });
+
+
+  describe('newComponentStructure = nested', function() {
+    var tmpPath = "tmp/process-files";
+    var fixturesPath = path.resolve(__dirname, "fixtures/classic-to-nested");
+
+    beforeEach(function() {
+      fse.mkdirsSync(tmpPath);
+    });
+
+    afterEach(function() {
+      fse.removeSync(tmpPath);
+    });
+
+    var entries = fse.readdirSync(fixturesPath);
+
+    entries.forEach(async function(entry) {
+      it(`should migrate ${entry} fixture properly`, async function() {
+        var fixturePath = path.join(fixturesPath, entry);
+        var input = require(fixturePath + "/input");
+        var expected = require(fixturePath + "/output");
+        var migratorConfig = {};
+        try {
+          migratorConfig = require(fixturePath + "/config");
+        } catch (e) {
+          // fixture uses default config...
+        }
+
+        fixturify.writeSync(tmpPath, input);
+
+        var migratorOptions = Object.assign(
+          {
+            projectRoot: tmpPath,
+            newComponentStructure: 'nested'
+          },
+          migratorConfig
+        );
+
+        var migrator = new Migrator(migratorOptions);
+        await migrator.execute();
+
+        var actual = fixturify.readSync(tmpPath);
+        assertDiff.deepEqual(actual, expected, "the codemod should work as expected");
+
+        await migrator.execute();
+        assertDiff.deepEqual(actual, expected, "the codemod should be idempotent");
+      });
+    });
+  });
 });
